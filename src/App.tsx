1e88a9722329ee8d153f0af1485536a48e400048
Joaquin Rampa <rampajoa@gmail.com>
2025-03-21 19:42:27 +0100

    composition pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import './App.css'
 import { useState } from 'react';
-import { Button } from './components'
-import { useFetch } from './hooks';
+import { Button, Button2, ColorRed } from './components'
+//import { useFetch } from './hooks';
 
-const url = 'https://jsonplaceholder.typicode.com/posts';
+//const url = 'https://jsonplaceholder.typicode.com/posts';
 
-interface Data {
+/*interface Data {
   name: string;
   lastName: string;
   age: number;
-}
+}*/
 
 function App() {
   const [count, setCount] = useState(0) //hooks (es un constructor de clase ) (state -> value, getValue{}, setValue)
-  const {data, error, loading} = useFetch<Data>(url);
+  //const {data, error, loading} = useFetch<Data>(url);
+  //const {data: data2, error: error2, loading: loading2} = useFetch<name: string>(url2);
+  //ya es posible usar el fetch generico y controlar el dato q se espera
 
-  const countMore = () => {
-    setCount((count) => count + 1)
-  }
-  
-if(loading) {
-  return <div>Cargando...</div>
-}
+  const countMore = () => setCount((count) => count + 1)
+  const handleClick = () => console.log("click")
 
   return (
     <>
       <Button label={`Count is ${count}`} parentMethod={countMore}/>
+      <Button2 parentMethod={handleClick}> 
+        <ColorRed><div>my label</div></ColorRed>
+      </Button2>
     </>
   )
 }
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,8 +1,19 @@
+import { JSX, ReactNode } from "react"
 import "./button.css"
 
 interface Props {
   label: string,
   parentMethod: () => void
+}
+interface Props2 {
+  children: ReactNode,
+  parentMethod: () => void
+}
+interface ChildrenProps {children: JSX.Element}
+
+//logica de poner el texto en color rojo, logica minima posible para cambiar las cosas al color
+export const ColorRed = ({children}: ChildrenProps) => {
+  return (<div style={{"color": "red"}}>{children}</div>)
 }
 
 //destrucutracion para atomizar el button a su menor expresion
@@ -11,6 +22,14 @@ export const Button = ({label, parentMethod}: Props) => {
   return (
     <button className='custom-button' onClick={parentMethod}>
       {label}
+    </button>
+  )
+}  
+export const Button2 = ({children, parentMethod}: Props2) => {
+  return (
+    //composition pattern
+    <button className='custom-button' onClick={parentMethod}>
+      {children}
     </button>
   )
 }
